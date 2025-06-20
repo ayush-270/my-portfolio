b2f40f361c484a23a3f34257f41e3dc081f97a40
ayush-270 <ayush27.work@gmail.com>
2025-06-20 12:44:41 +0530

    Header Component designed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import "./Header.css"
+import CTA from "./CTA"
+import ME from "../../assets/download.jpeg"
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="conatiner header__container">
+        <h5>Hello, I'm </h5>
+        <h1>Ayush Gupta</h1>
+        <h5 className="text-light">Web Developer</h5>
+        <CTA/>
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+        <a href="#contacts" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>   
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="http://linkedin.com" target="_blank"></a>
+      <a href="http://github.com" target="_blank"></a>
+      <a href="http://x.com" target="_blank"></a>
+    </div>
+  );
+}
+
+export default HeaderSocials
