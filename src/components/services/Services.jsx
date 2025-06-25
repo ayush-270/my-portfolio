e18e56141fe84746132694871764a75181717858
ayush-270 <ayush27.work@gmail.com>
2025-06-25 15:29:22 +0530

    `Added JSX and CSS styles for services component.`

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,11 +1,131 @@
 import React from 'react'
 import "./Services.css"
+import { FaCheck } from "react-icons/fa6";
 
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
-  )
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        {/* ===================UI/UX Design=================== */}
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+          </ul>
+        </article>
+        {/* ===================WEB DEVELOPMENT=================== */}
+        <article className="service">
+          <div className="service__head">
+            <h3>WEB Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+          </ul>
+        </article>
+        {/* ===================Content Creation=================== */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+
+            <li>
+              <FaCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Services
