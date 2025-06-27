98b3225794e8b31146d4afea3a922d0b360ed742
ayush-270 <ayush27.work@gmail.com>
2025-06-27 21:10:25 +0530

    `Added emailjs and swiper libraries for contact form and testimonials section`

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,10 +1,96 @@
 import React from 'react'
 import "./Contacts.css"
+import { MdOutlineEmail } from "react-icons/md";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa";
+
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contacts = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_eyby1zi", "template_u7v4e7j", form.current, {
+        publicKey: "6RUzS6bInjVvXeXbF",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          e.target.reset();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
-    <section id='contacts'>Contacts</section>
-  )
+    <section id="contacts">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>ayush27.work@gmail.com</h5>
+            <a href="mailto:ayush27.work@gmail.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <FaXTwitter className="contact__option-icon" />
+            <h4>X</h4>
+            <h5>Ayush Gupta</h5>
+            <a href="https://x.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <FaWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>ayush27.work@gmail.com</h5>
+            <a href=" https://wa.me/919355088572" target="_blank">
+              Send a Message
+            </a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            id=""
+            placeholder="Your Email"
+            required
+          />
+          <textarea
+            name="message"
+            id=""
+            rows={7}
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contacts
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,66 +3,72 @@ import "./Testimonials.css"
 import AVTR from "../../assets/avatar.jpeg"
 
 
+// import Swiper core and required modules
+import {Pagination} from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
+const data = [
+  {
+    avatar: AVTR,
+    name: "ABC",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium consequatur ipsum deserunt fugit debitis. Nostrum odit laborum excepturi consectetur aliquam earum, nihil dicta minus, enim culpa labore quaerat quos quis!",
+  },
+  {
+    avatar: AVTR,
+    name: "ABC",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium consequatur ipsum deserunt fugit debitis. Nostrum odit laborum excepturi consectetur aliquam earum, nihil dicta minus, enim culpa labore quaerat quos quis!",
+  },
+  {
+    avatar: AVTR,
+    name: "ABC",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium consequatur ipsum deserunt fugit debitis. Nostrum odit laborum excepturi consectetur aliquam earum, nihil dicta minus, enim culpa labore quaerat quos quis!",
+  },
+  {
+    avatar: AVTR,
+    name: "ABC",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium consequatur ipsum deserunt fugit debitis. Nostrum odit laborum excepturi consectetur aliquam earum, nihil dicta minus, enim culpa labore quaerat quos quis!",
+  },
+];
+
+
 const Testimonials = () => {
   return (
     <section id="testimonials">
       <h5>Review from Clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR} alt="Avatar Image" />
-          </div>
-          <h5 className="client__name">ABC</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem quibusdam aperiam dolorem quod voluptate, ipsum
-            eveniet assumenda nemo dolor natus numquam, voluptates sunt magnam
-            fugit ipsa illum placeat quisquam ex.
-          </small>
-        </article>
-
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR} alt="Avatar Image" />
-          </div>
-          <h5 className="client__name">ABC</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem quibusdam aperiam dolorem quod voluptate, ipsum
-            eveniet assumenda nemo dolor natus numquam, voluptates sunt magnam
-            fugit ipsa illum placeat quisquam ex.
-          </small>
-        </article>
-
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR} alt="Avatar Image" />
-          </div>
-          <h5 className="client__name">ABC</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem quibusdam aperiam dolorem quod voluptate, ipsum
-            eveniet assumenda nemo dolor natus numquam, voluptates sunt magnam
-            fugit ipsa illum placeat quisquam ex.
-          </small>
-        </article>
-
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR} alt="Avatar Image" />
-          </div>
-          <h5 className="client__name">ABC</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem quibusdam aperiam dolorem quod voluptate, ipsum
-            eveniet assumenda nemo dolor natus numquam, voluptates sunt magnam
-            fugit ipsa illum placeat quisquam ex.
-          </small>
-        </article>
-
-      </div>
+      <Swiper
+        className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="Avatar Image" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 }
